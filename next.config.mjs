b8e5966b06f8ef61f7676da6e3ed88f4e583ce30
feranmi78 <emmfe78@gmail.com>
2026-03-03 17:34:10 +0100

    fix: add next.config.mjs

--- next.config.mjs
+++ next.config.mjs
@@ -1,11 +1,7 @@
-import type { NextConfig } from "next";
-
-const nextConfig: NextConfig = {
+/** @type {import('next').NextConfig} */
+const nextConfig = {
   experimental: {
     serverComponentsExternalPackages: ["@prisma/client", "bcryptjs"],
-  },
-  images: {
-    remotePatterns: [],
   },
 };
 
